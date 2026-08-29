8ca3a5dde16c1e6cd8c40ef1b32f580aa708d6e1
S1NXIAN <xian.mainz@gmail.com>
2026-08-29 08:30:27 +0800

    perf(lazy): disable netrw, matchit, and matchparen runtime plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,9 @@ require("lazy").setup({
     rtp = {
       disabled_plugins = {
         "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
